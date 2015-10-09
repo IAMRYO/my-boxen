864b16e6857f3a7fdc9f14d8cc4d298f2e79efef
Ryan Clark <ryan@devmynd.com>
2015-10-09 14:18:33 -0700

    Remove all rubies from default install

--- manifests/site.pp
+++ manifests/site.pp
@@ -79,13 +79,13 @@ node default {
   nodejs::version { '0.10': }
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
-  ruby::version { '2.1.5': }
-  ruby::version { '2.2.2': }
+#  ruby::version { '1.9.3': }
+#  ruby::version { '2.0.0': }
+#  ruby::version { '2.1.0': }
+#  ruby::version { '2.1.1': }
+#  ruby::version { '2.1.2': }
+#  ruby::version { '2.1.5': }
+#  ruby::version { '2.2.2': }
 #  ruby::version { '2.2.3': }
 
   # common, useful packages
